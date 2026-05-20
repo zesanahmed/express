a860062bfdbaa6d7b0fb0956611c7c499714a466
zesanahmed <zesanahmed593@gmail.com>
2026-05-20 12:24:27 +0600

    server.ts added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,34 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+const app: Application = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_elAXaFx4r3LE@ep-frosty-fire-aq9vslrh-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Express server",
+    Author: "Zesan Ahmed",
+  });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  res.status(201).json({
+    message: "Created",
+    data: { name, email },
+  });
+  console.log(req.body);
 });
 
 app.listen(port, () => {
