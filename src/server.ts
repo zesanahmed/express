19f0e0358b4ec4f0f7bfd463c25e938fd3181ede
zesanahmed <zesanahmed593@gmail.com>
2026-05-21 00:00:47 +0600

    express delete method

--- src/server.ts
+++ src/server.ts
@@ -130,7 +130,12 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
       `
-    UPDATE users SET name=$1,password=$2,age=$3,is_active=$4
+    UPDATE users 
+    SET 
+    name=COALESCE($1, name),
+    password=COALESCE($2 ,password),
+    age=COALESCE($3, age),
+    is_active=COALESCE($4, is_active)
     WHERE id=$5 RETURNING *
     `,
       [name, password, age, is_active, id],
@@ -141,6 +146,37 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
         success: false,
         message: "User not found",
         data: {},
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Users updated successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
+app.delete("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `
+    DELETE FROM users WHERE id=$1
+    `,
+      [id],
+    );
+
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found",
       });
     }
     res.status(200).json({
